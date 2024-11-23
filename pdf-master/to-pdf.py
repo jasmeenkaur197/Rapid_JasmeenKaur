07b95f1bcd34a07a96e0bdf1cac88ac92f55420d
jasmeenkaur197 <jk685664@gmail.com>
2024-11-23 23:09:05 +0530

    default port settings changed

--- pdf-master/to-pdf.py
+++ pdf-master/to-pdf.py
@@ -74,7 +74,7 @@ def upload_file():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 if __name__ == "__main__":
